fd82788b9fdb71e1f54360cf15255eb52336916e
Randee <rshirts@gmail.com>
2017-10-15 14:33:49 -0600

    Fishished product

--- 6_Adapter/src/com/company/Shape.java
+++ 6_Adapter/src/com/company/Shape.java
@@ -1,9 +1,25 @@
 package com.company;
 
 public abstract class Shape {
-    public void setLocation(){
 
+    String location = "0,0";
+    String color = "Blue";
+
+    public abstract void display();
+    public abstract void fill();
+    public abstract void undisplay();
+
+    public void setLocation(String location){
+        System.out.println("Changing location from " + this.location + " to " + location);
+        this.location = location;
     }
 
-    public abstract void getLocation();
+    public String getLocation() {
+        return location;
+    }
+
+    public void setColor(String color) {
+        System.out.println("Changing color from " + this.color + " to " + color);
+        this.color = color;
+    }
 }
